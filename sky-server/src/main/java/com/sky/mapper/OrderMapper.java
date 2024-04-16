527c385ec9a56bc2c457874a3e816a445d2ffe6c
LeonDragon7 <209418465@qq.com>
2024-04-16 21:07:03 +0800

    feat: 统计指定时间区间内的营业额

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -3,12 +3,13 @@ package com.sky.mapper;
 import com.github.pagehelper.Page;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
-import com.sky.vo.OrderVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+
 
 @Mapper
 public interface OrderMapper {
@@ -54,4 +55,11 @@ public interface OrderMapper {
      * @return
      */
     Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 动态条件统计营业额数据
+     * @param map
+     * @return
+     */
+    Double sumByMap(Map<String, Object> map);
 }
